7c4f4f90369af064ad0c18566f5ada2885de4460
Cheng Koon <cann@ap.equinix.com>
2021-12-16 14:57:41 +0800

    change to all passing tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,8 +1,8 @@
 it('works', () => {
   expect(2 + 3).to.equal(5)
 })
-it('will fail', () => {
-  expect(3 + 4).to.equal(6)
+it('will pass', () => {
+  expect(3 + 4).to.equal(7)
 })
 it('will pass', () => {
   expect(5 + 6).to.equal(11)
